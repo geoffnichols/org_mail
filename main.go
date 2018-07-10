0d44b81a4fde7e3926f99e8aff1e32d6d603cc14
Michael Stahnke <stahnma@puppet.com>
2018-07-10 22:45:46 +0000

    Works and returns proper object now

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	//	"fmt"
 	"github.com/kr/pretty"
 	"github.com/spf13/viper"
 	"gopkg.in/mup.v0/ldap"
 	//	"strconv"
 	//	"reflect"
+	log "github.com/sirupsen/logrus"
+	"os"
 	"strings"
 )
 
@@ -15,7 +16,7 @@ import (
 func is_manager(conn ldap.Conn, uid string) bool {
 	manager_dn := "uid=" + uid + ",ou=users,dc=puppetlabs,dc=com"
 	search := &ldap.Search{
-    Filter: "(&(manager=" + manager_dn + ")(!(employeeType=Intern))(!(objectclass=exPuppetPerson)))",
+		Filter: "(&(manager=" + manager_dn + ")(!(employeeType=Intern))(!(objectclass=exPuppetPerson)))",
 		Attrs:  []string{"sn", "mail", "uid", "manager"},
 	}
 	results, err := conn.Search(search)
@@ -45,37 +46,53 @@ func who_has_this_manager(conn ldap.Conn, uid string) []string {
 	return dns
 }
 
-func shrink_dn(uid string) string {
+func shrink_dn(uid string) (string, string) {
+	dn := uid
 	if strings.Contains(uid, "dc=") {
 		idx := strings.Index(uid, "ou=")
 		uid = uid[0:idx]
 		uid = strings.Trim(uid, ",")
 		uid = strings.Replace(uid, "uid=", "", 1)
 	}
-	return uid
+	if strings.Contains(uid, "dc=") == false {
+		dn = "uid=" + uid + ",ou=users,dc=puppetlabs,dc=com"
+	}
+
+	return dn, uid
 }
 
 // need to rework to a struct vs string array
 func build_tree(conn ldap.Conn, uid string) []string {
-	uid = shrink_dn(uid)
+	log.Debug("In build tree, and uid string passed is " + uid)
+	var dn string
+	dn, uid = shrink_dn(uid)
 	peers := []string{}
 	if is_manager(conn, uid) {
+		log.Debug("In build tree, and uid " + uid + " is a manager")
 		for _, res := range who_has_this_manager(conn, uid) {
 			// evalute if any of these people are managers
-			pretty.Println(res)
+			//		pretty.Println(res)
 			//fmt.Println(reflect.TypeOf(res))
 			peers = append(peers, build_tree(conn, res)...)
+			//pretty.Println(build_tree(conn, res))
+			//pretty.Println(peers)
 		}
 		return peers
 
 	} else {
-		return nil
+		peers = append(peers, dn)
+		log.Debug("In build tree, and uid " + uid + " is *not* a manager")
+		return peers
 	}
 	return peers
 
 }
 
 func main() {
+	log.SetFormatter(&log.TextFormatter{})
+	log.SetOutput(os.Stdout)
+	//TODO log level configurable via envvar
+	log.SetLevel(log.InfoLevel)
 
 	viper.BindEnv("LDAP_USERNAME")
 	viper.BindEnv("LDAP_PASSWORD")
@@ -101,6 +118,6 @@ func main() {
 	//  fmt.Println(reflect.TypeOf(conn))
 	//	pretty.Println(who_has_this_manager(conn, "erict"))
 	//	pretty.Println(is_manager(conn, "bradejr"))
-	build_tree(conn, "stahnma")
+	pretty.Println(build_tree(conn, "stahnma"))
 
 }
